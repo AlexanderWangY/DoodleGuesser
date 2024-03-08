086d517e7fa1a39f3efb9c47a0e0a934c67caa8d
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-03-07 20:38:25 -0500

    added flask and started server

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,26 @@ function App() {
     canvas.current.clearCanvas()
   }
 
+  const handleSubmit = async () => {
+    const drawing = await canvas.current.exportImage();
+    console.log(drawing)
+
+    const body = JSON.stringify({
+      'img': drawing
+    })
+
+    const response = await fetch("http://localhost:8080/guess", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: body
+    })
+    const data = await response.json()
+
+    console.log(data.prediction)
+  }
+
   return (
     <div className="main-container">
       <div className="header">
@@ -34,7 +54,7 @@ function App() {
         </div>
         <div className="button-container">
           <button className="button" onClick={handleClear}>Clear</button>
-          <button className="button">Submit</button>
+          <button className="button" onClick={handleSubmit}>Submit</button>
         </div>
       </div>
     </div>
--- server/main.py
+++ server/main.py
@@ -1,1 +1,19 @@
-# This is where the server will be!
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from predict import infer
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/guess", methods=['POST'])
+def predict():
+    data = request.json
+    img_base = data["img"]
+
+    result = infer(img_base)
+
+    return jsonify({
+        'prediction': result
+    })
+
+app.run(host="0.0.0.0", port=8080)
